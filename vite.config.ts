1da71e4824a6e39642e9bc80d2d026404476cf66
Dinesh Patel <dinesh.24jiaiml037@jietjodhpur.ac.in>
2026-06-14 19:02:48 +0530

    Fix Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      server: { entry: "src/server.ts" },
-    }),
     react(),
     tsconfigPaths(),
     tailwindcss(),
